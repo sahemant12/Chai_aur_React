b4d5e7c73bb8e83dee475cdfb08cf2512274320a
Hemant Sah <hemantsah2912@gmail.com>
2025-05-23 01:15:05 +0530

    update till-04

---
+++ 03-counterProject/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // let counter = 5;
+  // function increment(){
+  //   counter++;
+  //   console.log(counter); 
+  // }
+  // function decrement(){
+  //   counter--;
+  //   console.log(counter);
+  // } //these will not update the ui. For that we use hook: useState
+  
+  
+  let [counter, setCounter] = useState(5);
+
+  function increment(){
+    if(counter<10){
+      setCounter(counter+1);
+    }
+  }
+  function decrement(){
+    if(counter>0){
+      setCounter(counter-1);
+    }
+  }
+
+  return (
+    <>
+      <h2>Lets make counter by ownself: {counter}</h2>
+      <h2>Counter: {counter}</h2>
+      <button onClick={increment}>+</button><p>{counter}</p>
+      <hr />
+      <button onClick={decrement}>-</button>
+    </>
+  )
+}
+
+export default App
---
+++ 04-tailwind-props/src/App.jsx
@@ -0,0 +1,19 @@
+import Card from "./components/card.jsx"
+function App() {
+  let mark = 25;
+  let newObj = {
+    age:12,
+    gender: "male",
+  }
+  let arr = [1, 2, 3];
+
+
+  return (
+    <>
+      <Card name="Hemant" marks={mark} myObj={newObj} num={arr}/>
+      <Card />
+    </>
+  )
+}
+
+export default App;
