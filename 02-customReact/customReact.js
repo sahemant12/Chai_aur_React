753cc96c5c76817f8d6d6745a064583d143be68d
Hemant Sah <hemantsah2912@gmail.com>
2025-07-27 22:25:16 +0530

    just update

--- 02-customReact/customReact.js
+++ 02-customReact/customReact.js
@@ -1,4 +1,7 @@
+// In this program we are making our own React.
 
+
+// 1. This is Component/fn that we get to render in root element. This is how React see the component element.
 const reactElement = {
     type: "a",
     props: {
@@ -7,8 +10,39 @@ const reactElement = {
     },
     children: "click me to visit google",
 }
-
-
-
+function Convert(){ // this convert into above at the end after parse.
+    return(
+        <><a target="_blank" href="https://google.com">click me to visit google</a></>
+    )
+}
+// 2. This is the root where we will render our all component.
 const mainContainer = document.getElementById("root");
 
+
+// 3. Make a render fn that render the component to the root element.
+customRender(reactElement, mainContainer); // 1st argu: component that will render. // 2nd argu: place where we want to render component.
+
+function customRender(reactElement, container){
+
+    //good, but not optimize code
+    // const domElement = document.createElement(reactElement.type);
+    // domElement.innerHTML = reactElement.children;
+    // domElement.setAttribute("href", reactElement.props.href);
+    // domElement.setAttribute("target", reactElement.props.target);
+
+    // container.appendChild(domElement);
+
+
+    //optimize code
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children;
+
+    for(let prop in reactElement.props){
+        domElement.setAttribute(prop, reactElement.props[prop]);
+    }
+    container.appendChild(domElement);
+}
+
+// 4. Bundler help to convert syntax of fn into component syntax.
+// E.g: function App(){return (<></>)} ----->   <App/>
+// Also, after parse this fn converted into above object.
---
+++ notes.js
@@ -0,0 +1,42 @@
+// #################    Notes     ###################
+
+// 1. vite is a blundler.
+// 2. ui updation ko react control karta hai.
+// 3. With every project make a good `Readme` file. Sir have better template for readme file get it.
+
+
+// Lecture-3(Folder: 01): Understand the react flow and structure
+
+// 1. react => react-dom(for website), react-native(for mobile app).
+//    react-dom is the implementation of react on web whereas react-native is the implementation of react on mobile app.
+
+// 2. browser have its own DOM. Likewise, React have also its DOM known as `virtual DOM`. It get compared with main DOM and main DOM get changes according to the virtual DOM.
+
+// 3. React at the end of the day is Js. Library of Js. Okay React is coming from here, this is how React injected into the project, injecting at this point, now my application is getting ready. __ undertsand the react flow and structure.
+
+// 4. "react-scripts" present in dependencies in `package.json` files help to inject the `index.js` file(main file) into the HTML file.
+
+// 5. in `main React app` we can make component as `comp.js` but in vite React app we have to make component as `comp.jsx`
+//6. name of fn/component should start with capital letter.
+
+
+
+// Lecture-4(Folder: 02-customReact): Create your own react library and JSX
+
+// 1. You can watch the lecture again for fast revision: https://www.youtube.com/watch?v=kAOuj6o7Kxs&list=PLu71SKxNbfoDqgPchmvIsL4hTnJIrtige&index=5
+
+// 2. At the end React also made tree like structure for elements and code.
+
+
+
+// Lecture-5(Folder: 03-counterProject): Why you need hooks and project
+
+// 1. React help in UI updation, thats one reason why we need hook in React.
+
+
+// Lecture-6: Virtual DOM, Fibre, and Reconciliation*******
+ // 1. Now, virtual DOM is not used in React.
+ // 2. 
+
+
+ // Lecture-7: 
